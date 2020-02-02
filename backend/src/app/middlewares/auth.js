5d921af783f4a08f588c2ad53e1888fe0fb0783d
Henrique Pagno <henriquepagno@gmail.com>
2020-02-02 14:14:29 -0300

    First commit

---
+++ backend/src/app/middlewares/auth.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import { promisify } from 'util';
+
+import authConfig from '../../config/auth';
+
+export default async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: 'Token not provided' });
+  }
+
+  // Splits string and get just the second position, since the word "Bearer" will not be necessary
+  const [, token] = authHeader.split(' ');
+
+  try {
+    // Promisify transforms a callback function into an asynchronous function.
+    const decoded = await promisify(jwt.verify)(token, authConfig.secret);
+
+    req.userId = decoded.id;
+
+    return next();
+  } catch (err) {
+    return res.status(401).json({ error: 'Invalid token' });
+  }
+};
---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '5022d242219adb12ab5fb786629320fd',
+  expiresIn: '7d',
+};
