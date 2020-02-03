7f79b89f92cd23b40039feeb04f30f0d4aba1eaf
Henrique Pagno <henriquepagno@gmail.com>
2020-02-02 23:56:13 -0300

    Created recipient model and migration

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hellooooo' });
-});
+routes.post('/sessions', SessionController.store);
+
+// Defines global authorization middleware to the following routes below.
+routes.use(authMiddleware);
+
+// routes.post();
+// routes.put();
 
 export default routes;
