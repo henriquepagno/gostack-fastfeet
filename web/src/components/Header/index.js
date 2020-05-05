5003a78472eb6dcefda8e2f01ccef30dc2cb8d49
Henrique Pagno <henriquepagno@gmail.com>
2020-05-05 19:58:51 -0300

    Added sign out functionality to the header

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import history from '~/services/history';
+
+import { signOut } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/fastfeet-logo.png';
 import { Container, Content, Profile, StyledLink } from './styles';
 
 export default function Header() {
   const profile = useSelector((state) => state.user.profile);
-
   const currentPathname = history.location.pathname;
+  const dispatch = useDispatch();
+
+  function handleSignOut() {
+    dispatch(signOut());
+  }
 
   return (
     <Container>
@@ -54,7 +60,9 @@ export default function Header() {
           <Profile>
             <div>
               <strong>{profile.name}</strong>
-              <button type="button">Sair do sistema</button>
+              <button type="button" onClick={handleSignOut}>
+                Sair do sistema
+              </button>
             </div>
           </Profile>
         </aside>
